51b390ada2d2d6c3d40098fe75cc8a681647b621
tatsukoohno1441 <134580838+tatsukoohno1441@users.noreply.github.com>
2025-09-19 17:44:15 +0900

    add: 新增本店店铺

--- make_outbound.py
+++ make_outbound.py
@@ -30,6 +30,7 @@ STORE_LIST = [
     "販売一丁目（楽天）",
     "ニューライフ",
     "販売一丁目 Wowma店",
+    "販売一丁目【本店】",
 ]
 HEADER = ["存货编码", "仓库", "数量", "单价", "SN码", "备注"]
 ERROR_FILL = PatternFill("solid", fgColor="CCE5FF")  # 蓝
